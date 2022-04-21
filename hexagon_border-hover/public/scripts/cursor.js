e7beafbe899c4f3aa3ad3e9a702d882c7117eb54
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-04-21 08:21:42 -0300

    feat: First Class: How To Create Cube Using CSS Clip-Path

--- hexagon_border-hover/public/scripts/cursor.js
+++
@@ -1,8 +0,0 @@
-const cursor = document.querySelector('#cursor');
-
-const onMouseChangePosition = (event) => {
-  cursor.style.left = event.clientX + 'px';
-  cursor.style.top = event.clientY + 'px';
-};
-
-onmousemove = onMouseChangePosition
